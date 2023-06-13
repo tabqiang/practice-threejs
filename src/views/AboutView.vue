3e88796090f3a4248c0fc78da876c01202023882
Caps <838269741@qq.com>
2023-06-13 23:08:00 +0800

    chore(config): prettier,lint

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,49 +3,51 @@
 </template>
 
 <script setup>
-import * as THREE from "three";
-import { GUI } from "dat.gui";
+import * as THREE from 'three'
+import { GUI } from 'dat.gui'
 
-// 实例化GUI
-const gui = new GUI();
-// init
+// initialize the tools
+const gui = new GUI()
 
+// 1. Create scene
+const scene = new THREE.Scene()
+
+// 2. Create camera
 const camera = new THREE.PerspectiveCamera(
   70,
   window.innerWidth / window.innerHeight,
   0.01,
   10
-);
-camera.position.z = 1;
+)
+camera.position.z = 1
 
-const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry(0.3, 0.3, 0.3)
 
-const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-const material = new THREE.MeshNormalMaterial();
+const material = new THREE.MeshNormalMaterial()
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 gui
-  .add(scene.rotation, "x")
+  .add(scene.rotation, 'x')
   .min(0)
   .max(100)
-  .name("x轴")
-  .onChange((value) => {
-    console.log(value);
-  });
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animation);
-document.body.appendChild(renderer.domElement);
+  .name('x轴')
+  .onChange(value => {
+    console.log(value)
+  })
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.setSize(window.innerWidth, window.innerHeight)
+// renderer.setAnimationLoop(animation);
+document.body.appendChild(renderer.domElement)
 
 // animation
 
 function animation(time) {
-  mesh.rotation.x = time / 2000;
-  mesh.rotation.y = time / 2000;
+  mesh.rotation.x = time / 2000
+  mesh.rotation.y = time / 2000
 
-  renderer.render(scene, camera);
+  renderer.render(scene, camera)
 }
 </script>
 
